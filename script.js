778fae73dfc5a453a9aa00edc04023330021f3eb
Aregbe Ayatulah Adekunle <144852736+Stoner1238@users.noreply.github.com>
2025-08-08 11:41:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ document.getElementById("openSidebar").addEventListener("click", function () {
 document.getElementById("closeSidebar").addEventListener("click", function () {
   document.getElementById("sidebar").classList.remove("active");
 });
+// Highlight the active link
+const links = document.querySelectorAll(".sidebar a");
+const currentPage = window.location.pathname.split("/").pop();
+
+links.forEach(link => {
+  if (link.getAttribute("href") === currentPage) {
+    link.classList.add("active");
+  }
+});
+
